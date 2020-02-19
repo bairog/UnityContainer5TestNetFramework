0bfa8866a5c38b0ad31eb0f95093d221a243233a
bairog <44995181+bairog@users.noreply.github.com>
2020-02-19 08:09:02 +0300

    First commit

---
+++ Unity5ContainerTest/Implementations/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementations
+{
+    public class ImplementationN : InterfaceN
+    {
+        public string GetString()
+        {
+            return "Hello world!";
+        }
+    }
+}
---
+++ Unity5ContainerTest/Unity5ContainerTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using Microsoft.Practices.Unity.Configuration;
+using Interfaces;
+
+namespace Unity5ContainerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new UnityContainer().LoadConfiguration();
+
+            var type = container.Resolve<InterfaceN>();
+
+            Console.WriteLine(type.GetString());
+            Console.ReadKey();
+        }
+    }
+}
